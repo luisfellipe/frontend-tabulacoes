1761d9c1242c1c7d8de94deeaf994df9a402d2ff
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-06-27 21:14:50 -0300

    devCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ type SignInFormData = {
   password: string;
 }
 
-// Yup schema - validações do login
+// Yup schema 
 const signInFormSchema = yup.object().shape({
   email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
   password: yup.string().required('Senha obrigatória'),
