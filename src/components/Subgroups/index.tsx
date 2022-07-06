67d6c11516203b58d71c6a5d3735ccc892bc1ac6
Caio Vinícius <caio1525pereira@gmail.com>
2022-07-06 12:09:46 -0300

    feat: Drag and Drop

--- src/components/Subgroups/index.tsx
+++ src/components/Subgroups/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Box, Flex, Icon, Text } from "@chakra-ui/react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { RiAddLine } from "react-icons/ri";
 import { BiTrash } from "react-icons/bi";
 
 type ItemSubgroups = {
+  id: string;
   item: string;
 };
 
@@ -11,67 +14,95 @@ interface SubgroupsProps {
 }
 
 export function Subgroups({ subgroup }: SubgroupsProps) {
+  const [items, setItems] = useState<ItemSubgroups[]>(subgroup);
+
+  function handleOnDragEnd(result) {
+    const newItemsArray = Array.from(items);
+    const [reorderedItem] = newItemsArray.splice(result.source.index, 1);
+    newItemsArray.splice(result.destination.index, 0, reorderedItem);
+
+    setItems(newItemsArray);
+    console.log(newItemsArray);
+  }
+
   return (
-    <>
-      {subgroup.map((subgroup: ItemSubgroups) => {
-        return (
-          <Flex
-            key={subgroup.item}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            gap="5px"
-          >
-            <Box
-              bg="colorBackground.typeAndItem"
-              margin="5px"
-              borderRadius="5px"
-              width="100%"
-              display="flex"
-              justifyContent="space-between"
-            >
-              <Text
-                p="12px"
-                color="colorText.textTable"
-                fontSize={["10px", "12px", "14px"]}
-                textAlign="center"
-              >
-                {subgroup.item}
-              </Text>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="items">
+        {(provided) => (
+          <Box {...provided.droppableProps} ref={provided.innerRef}>
+            {items.map((subgroup: ItemSubgroups, index) => {
+              return (
+                <Draggable
+                  key={subgroup.id}
+                  draggableId={subgroup.id}
+                  index={index}
+                >
+                  {(provided) => (
+                    <Flex
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center"
+                      gap="5px"
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <Box
+                        bg="colorBackground.typeAndItem"
+                        margin="5px"
+                        borderRadius="5px"
+                        width="100%"
+                        display="flex"
+                        justifyContent="space-between"
+                      >
+                        <Text
+                          p="12px"
+                          color="colorText.textTable"
+                          fontSize={["10px", "12px", "14px"]}
+                          textAlign="center"
+                        >
+                          {subgroup.item}
+                        </Text>
 
-              <Box display="flex" alignItems="center" margin="8px">
-                <Icon
-                  as={BiTrash}
-                  color="colorText.subtractButton"
-                  fontSize={["0.7rem", "1.2rem"]}
-                  borderRadius="4px"
-                  mr="8px"
-                  cursor="pointer"
-                  textColor="colorText.deleteButton"
-                  _hover={{
-                    color: "colorText.deleteButtonHover",
-                  }}
-                  transition="0.2s"
-                ></Icon>
+                        <Box display="flex" alignItems="center" margin="8px">
+                          <Icon
+                            as={BiTrash}
+                            color="colorText.subtractButton"
+                            fontSize={["0.7rem", "1.2rem"]}
+                            borderRadius="4px"
+                            mr="8px"
+                            cursor="pointer"
+                            textColor="colorText.deleteButton"
+                            _hover={{
+                              color: "colorText.deleteButtonHover",
+                            }}
+                            transition="0.2s"
+                          ></Icon>
 
-                <Icon
-                  as={RiAddLine}
-                  color="white"
-                  fontSize={["0.7rem", "1.2rem"]}
-                  borderRadius="4px"
-                  cursor="pointer"
-                  textColor="colorText.addButton"
-                  _hover={{
-                    color: "colorText.addButtonHover",
-                    background: "colorBackground.addButtonHover",
-                  }}
-                  transition="0.2s"
-                ></Icon>
-              </Box>
-            </Box>
-          </Flex>
-        );
-      })}
-    </>
+                          <Icon
+                            as={RiAddLine}
+                            color="white"
+                            fontSize={["0.7rem", "1.2rem"]}
+                            borderRadius="4px"
+                            cursor="pointer"
+                            textColor="colorText.addButton"
+                            _hover={{
+                              color: "colorText.addButtonHover",
+                              background: "colorBackground.addButtonHover",
+                            }}
+                            transition="0.2s"
+                          ></Icon>
+                        </Box>
+                      </Box>
+                    </Flex>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </Box>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
